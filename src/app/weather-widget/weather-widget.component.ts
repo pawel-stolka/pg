6ab123e1869d7af75c8a55758f2b37409d9258da
Paweł Stolka (SII) <pstolka@sii.pl>
2023-02-09 06:48:20 +0200

    afterViewInit template loaded

--- src/app/weather-widget/weather-widget.component.ts
+++ src/app/weather-widget/weather-widget.component.ts
@@ -1,4 +1,10 @@
-import { Component, inject } from '@angular/core';
+import {
+  Component,
+  inject,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { WidgetActions } from '../widget-actions';
 import { WidgetState } from '../widget-state';
 
@@ -10,4 +16,12 @@ import { WidgetState } from '../widget-state';
 export class WeatherWidgetComponent {
   state = inject(WidgetState);
   actions = inject(WidgetActions);
+
+  @ViewChild('container', { read: ViewContainerRef })
+  container!: ViewContainerRef;
+  @ViewChild('defaultWidgetHeader') headerTemplate!: TemplateRef<any>;
+
+  ngAfterViewInit() {
+    this.container.createEmbeddedView(this.headerTemplate);
+  }
 }
