50ca0e80b5bd354d96b87a05d5b7e4bb37cec92b
Paweł Stolka (SII) <pstolka@sii.pl>
2023-05-05 20:10:32 +0200

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { WeatherWidgetComponent } from './weather-widget/weather-widget.component';
 import { WidgetActions } from './widget-actions';
 import { WidgetState } from './widget-state';
 
+const routes: Routes = []
+
 @NgModule({
   declarations: [AppComponent, WeatherWidgetComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [WidgetState, WidgetActions],
   bootstrap: [AppComponent],
 })
