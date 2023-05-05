25526a94a4d79959b15c1e667b9bae8bdad2833f
Paweł Stolka (SII) <pstolka@sii.pl>
2023-05-05 21:26:30 +0200

    add basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router'
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WeatherWidgetComponent } from './weather-widget/weather-widget.component';
 import { WidgetActions } from './widget-actions';
 import { WidgetState } from './widget-state';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailsComponent } from './details/details.component';
+import { NavComponent } from './nav/nav.component';
 
-const routes: Routes = []
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'details', component: ProductDetailsComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  declarations: [AppComponent, WeatherWidgetComponent],
+  declarations: [
+    AppComponent,
+    WeatherWidgetComponent,
+    HomeComponent,
+    ProductsComponent,
+    ProductDetailsComponent,
+    NavComponent,
+  ],
   imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [WidgetState, WidgetActions],
   bootstrap: [AppComponent],
