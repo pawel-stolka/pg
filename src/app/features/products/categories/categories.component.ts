ca9e8277269885e0704b896807b9a9ec3cee07b6
Paweł Stolka (SII) <pstolka@sii.pl>
2023-05-06 17:54:58 +0200

    basic html select with mocks

--- src/app/features/products/categories/categories.component.ts
+++ src/app/features/products/categories/categories.component.ts
@@ -1,23 +1,68 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Observable, delay, of } from 'rxjs';
 import { Category } from 'src/app/services/product.service';
 
 @Component({
   selector: 'categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CategoriesComponent {
   @Input() categories: Category[] = [];
-  animalControl = new FormControl<Animal | null>(null, Validators.required);
-  selectFormControl = new FormControl('', Validators.required);
-  animals: Animal[] = [
-    { name: 'Dog', sound: 'Woof!' },
-    { name: 'Cat', sound: 'Meow!' },
-    { name: 'Cow', sound: 'Moo!' },
-    { name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!' },
-  ];
+
+  durations$: Observable<string[]>;
+
+  durationForm: FormControl = new FormControl();//'bla', Validators.required);
+
+  animalsSub: any = null;
+  animals: string[] = [];
+  animalCtrl: FormControl = new FormControl();
+  // animals = mockAnimals;
+  // animalControl = new FormControl<Animal>(this.animals[0], Validators.required);
+
+  constructor() {
+    this.durations$ = this.getDurations();
+  }
+  ngOnInit(): void {
+    this.animalsSub = this.getAnimals().subscribe(res => {
+      // console.log('this.getAnimals | SUB', res);
+
+      this.animals = res;
+      console.log('this.animals', this.animals);
+      //Not Working
+      // this.animalCtrl.setValue('Cat');
+
+      //Working
+      // setTimeout(() => {
+      //   this.animalCtrl.setValue('Cat');
+      // }, 0);
+    });
+  }
+
+  getAnimals = (): Observable<any> => {
+    return of(mockAnimals).pipe(delay(1500));
+  };
+
+  getDurations = (): Observable<string[]> => of(mockDurations).pipe(delay(1500));
+
 }
+
+const mockDurations: string[] = [
+  '3', '5', '8', '11'
+]
+
+const mockAnimals: string[] = [
+  'Lion', 'Tiger', 'Dog', 'Cat'
+]
+
+const mockAnimals0: Animal[] = [
+  { name: 'Dog', sound: 'Woof!' },
+  { name: 'Cat', sound: 'Meow!' },
+  { name: 'Cow', sound: 'Moo!' },
+  { name: 'Fox', sound: 'Wa-pa-pa-pa-pa-pa-pow!' },
+];
 
 interface Animal {
   name: string;
